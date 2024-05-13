16cf6e45bd2352c94bcd91b67b49fb3ab8f21b61
TimK <tim.karlowitz@gmail.com>
2024-05-13 16:50:55 +0200

    chore: Add relative positioning to footer

--- Components/Footer.js
+++ Components/Footer.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="py-12 sm:py-16 bg-black">
+    <footer className="relative py-12 sm:py-16 bg-black">
       <div className="px-6 mx-auto sm:px-8 lg:px-12 max-w-7xl">
         <div className="grid items-center grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-3">
           <div className="md:order-1">
