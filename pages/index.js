c49f950e33ea1228c13396595ed3c570d327bcba
TimK <tim.karlowitz@gmail.com>
2024-05-21 23:06:21 +0200

    chore: Update landing page with meta tags and favicon

--- pages/index.js
+++ pages/index.js
@@ -91,9 +91,9 @@ export default function Home() {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M4 6h16M4 12h16m-7 6h7"
                   />
                 </svg>
@@ -105,9 +105,9 @@ export default function Home() {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
@@ -116,7 +116,7 @@ export default function Home() {
               <a
                 href="https://play.google.com/store/apps/details?id=com.karlowitz.DreamShape"
                 title=""
-                className="hidden lg:inline-flex items-center justify-center px-5 py-2.5 text-base font-semibold text-white border-2 border-white hover:bg-gray-300 hover:text-white transition-all duration-200 focus:bg-black focus:text-white"
+                className="hidden lg:inline-flex items-center justify-center px-5 py-2.5 text-base font-semibold text-white border-2 border-white hover:bg-gray-300 hover:text-black transition-all duration-200 focus:bg-black focus:text-white"
                 role="button"
               >
                 {" "}
@@ -134,12 +134,14 @@ export default function Home() {
               <div className="flex flex-col justify-between flex-1 h-full">
                 <div>
                   <h1 className="text-4xl font-bold text-white sm:text-6xl xl:text-7xl">
-                    Take control <br />
-                    of your dreams
+                    Discover the Hidden Messages
+                    <br />
+                    in Your Dreams
                   </h1>
                   <p className="mt-6 text-base text-gray-400 sm:text-xl">
-                    Our A.I helps you to interpret your dreams based on your
-                    previous activity and helps you keep track of your dream.
+                    Unlock the secrets of your subconscious with Dreamshape. Our
+                    AI-powered app helps you interpret and understand your
+                    dreams like never before.
                   </p>
                   <a
                     href="https://play.google.com/store/apps/details?id=com.karlowitz.DreamShape"
@@ -159,19 +161,6 @@ export default function Home() {
                     </p>
 
                     <div className="flex items-center mt-5 space-x-5 sm:mt-0">
-                      <a
-                        href="#" // Replace this with your actual App Store link
-                        title="App Store Link"
-                        className=""
-                        role="button"
-                      >
-                        <img
-                          className="w-auto rounded h-14 sm:h-16"
-                          src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/4/app-store-button.png"
-                          alt="Download on the App Store"
-                        />
-                      </a>
-
                       <a
                         href="https://play.google.com/store/apps/details?id=com.karlowitz.DreamShape"
                         title="Play Store Link"
@@ -208,12 +197,12 @@ export default function Home() {
               </span>
             </p>
             <h2 className="mt-6 text-3xl  text-white sm:text-4xl lg:text-5xl xl:text-6xl font-semibold">
-              All dreams - One app
+              Every Dream Tells a Story
             </h2>
             <p className="mt-6 text-lg font-normal text-gray-400">
-              We wanted to create a safe space for YOU to keep track of and
-              explore your dreams. Dreamshape offers a dream journal app that
-              helps you to gain a deeper understanding of your inner self.
+              Dreamshape is your companion in exploring the fascinating world of
+              dreams. Keep track, interpret, and gain insights into your inner
+              self.
             </p>
           </div>
 
@@ -223,23 +212,23 @@ export default function Home() {
                 <div className="absolute inset-0 transition-all duration-200 rounded-xl group-hover:-inset-px bg-gradient-to-r from-gray-50 to-gray-100"></div>
                 <div className="relative inline-flex items-center justify-center w-24 h-24 rounded-xl bg-base-900">
                   <svg
-                    className="w-12 h-12 text-gray-400 transition-all duration-200 group-hover:text-white"
+                    className="w-12 h-12 text-gray-400 transition-all duration-200 group-hover:text-black"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="1"
+                    strokeWidth="1"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
                     />
                   </svg>
                 </div>
               </div>
               <h3 className="mt-6 text-lg font-normal text-white">
-                Personalized Analytics
+                Personalized Insights
               </h3>
             </div>
 
@@ -248,23 +237,23 @@ export default function Home() {
                 <div className="absolute inset-0 transition-all duration-200 rounded-xl group-hover:-inset-px bg-gradient-to-r from-gray-50 to-gray-100"></div>
                 <div className="relative inline-flex items-center justify-center w-24 h-24 rounded-xl bg-base-900">
                   <svg
-                    className="w-12 h-12 text-gray-400 transition-all duration-200 group-hover:text-white"
+                    className="w-12 h-12 text-gray-400 transition-all duration-200 group-hover:text-black"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="1"
+                    strokeWidth="1"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
                     />
                   </svg>
                 </div>
               </div>
               <h3 className="mt-6 text-lg font-normal text-white">
-                Unlimited Dreams
+                Unlimited Dream Entries
               </h3>
             </div>
 
@@ -273,23 +262,23 @@ export default function Home() {
                 <div className="absolute inset-0 transition-all duration-200 rounded-xl group-hover:-inset-px bg-gradient-to-r from-gray-50 to-gray-100"></div>
                 <div className="relative inline-flex items-center justify-center w-24 h-24 rounded-xl bg-base-900">
                   <svg
-                    className="w-12 h-12 text-gray-400 transition-all duration-200 group-hover:text-white"
+                    className="w-12 h-12 text-gray-400 transition-all duration-200 group-hover:text-black"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="1"
+                    strokeWidth="1"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
                 </div>
               </div>
               <h3 className="mt-6 text-lg font-normal text-white">
-                Notifications
+                Instant Notifications
               </h3>
             </div>
 
@@ -298,23 +287,23 @@ export default function Home() {
                 <div className="absolute inset-0 transition-all duration-200 rounded-xl group-hover:-inset-px bg-gradient-to-r from-gray-50 to-gray-100"></div>
                 <div className="relative inline-flex items-center justify-center w-24 h-24 rounded-xl bg-base-900">
                   <svg
-                    className="w-12 h-12 text-gray-400 transition-all duration-200 group-hover:text-white"
+                    className="w-12 h-12 text-gray-400 transition-all duration-200 group-hover:text-black"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="1"
+                    strokeWidth="1"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"
                     />
                   </svg>
                 </div>
               </div>
               <h3 className="mt-6 text-lg font-normal text-white">
-                Custom Categorisation
+                Custom Categorization
               </h3>
             </div>
 
@@ -323,23 +312,23 @@ export default function Home() {
                 <div className="absolute inset-0 transition-all duration-200 rounded-xl group-hover:-inset-px bg-gradient-to-r from-gray-50 to-gray-100"></div>
                 <div className="relative inline-flex items-center justify-center w-24 h-24 rounded-xl bg-base-900">
                   <svg
-                    className="w-12 h-12 text-gray-400 transition-all duration-200 group-hover:text-white"
+                    className="w-12 h-12 text-gray-400 transition-all duration-200 group-hover:text-black"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="1"
+                    strokeWidth="1"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                     />
                   </svg>
                 </div>
               </div>
               <h3 className="mt-6 text-lg font-normal text-white">
-                Chat Service
+                Live Chat Support
               </h3>
             </div>
           </div>
@@ -374,7 +363,7 @@ export default function Home() {
               </svg>
               <blockquote className="mt-8">
                 <p className="text-2xl font-medium leading-snug tracking-tight text-gray-900 sm:text-3xl">
-                  “Since I started using Dreamshape, Ive gained profound
+                  “Since I started using Dreamshape, I’ve gained profound
                   insights into my subconscious mind.“
                 </p>
                 <p className="mt-8 text-lg font-normal leading-8 text-gray-600 sm:text-xl sm:leading-9">
@@ -406,20 +395,6 @@ export default function Home() {
                     mood, and complete interpretation without procrastinating.
                   </p>
                   <div className="flex flex-col items-center mt-12 space-y-5 sm:space-y-0 sm:space-x-5 sm:flex-row sm:justify-center lg:justify-start sm:mt-16">
-                    <a
-                      href="#"
-                      title=""
-                      className="transition-all duration-200 hover:-translate-y-1"
-                      target="_blank"
-                      rel="noopener"
-                    >
-                      <img
-                        className="object-cover w-auto h-16"
-                        src="https://landingfoliocom.imgix.net/store/collection/saasui/images/application/2/app-store.png"
-                        alt=""
-                      />
-                    </a>
-
                     <a
                       href="https://play.google.com/store/apps/details?id=com.karlowitz.DreamShape"
                       title=""
